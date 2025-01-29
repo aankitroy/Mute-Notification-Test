41cad3a126ea088d92b491fd2cd635fe156ebaf8
jay-isampelliwar <isampelliwar79@gmail.com>
2025-01-29 10:54:14 +0530

    Added iOS native code for mute notification

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,39 @@
 import Flutter
 import UIKit
+import AVFoundation
+import MediaPlayer
+import AudioToolbox
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+   
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+     
+    let controller = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "notification_sound_channel", binaryMessenger: controller.binaryMessenger)
+     
+    channel.setMethodCallHandler { (call, result) in
+      if call.method == "reduceNotificationSound" {
+        self.reduceNotificationSoundAndVibration()
+        result(nil)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
+     
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+   
+  private func reduceNotificationSoundAndVibration() {
+    
+    DispatchQueue.main.async {
+      let volumeView = MPVolumeView()
+      if let volumeSlider = volumeView.subviews.first(where: { $0 is UISlider }) as? UISlider {
+        volumeSlider.value = 0.0
+      }
+    }
   }
 }
